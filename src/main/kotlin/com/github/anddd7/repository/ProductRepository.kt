c1302fa3238131f2e5b3c367c6f8d94a98ffd4c8
Anddd7 <liaoad_space@sina.com>
2020-01-22 15:25:32 +0800

    use reactive curd repo interface

--- src/main/kotlin/com/github/anddd7/repository/ProductRepository.kt
+++ src/main/kotlin/com/github/anddd7/repository/ProductRepository.kt
@@ -1,24 +1,8 @@
 package com.github.anddd7.repository
 
 import com.github.anddd7.entity.Product
-import org.springframework.data.r2dbc.core.DatabaseClient
-import org.springframework.data.r2dbc.core.from
-import org.springframework.data.r2dbc.query.Criteria.where
+import org.springframework.data.repository.reactive.ReactiveCrudRepository
 import org.springframework.stereotype.Repository
-import reactor.core.publisher.Flux
-import reactor.core.publisher.Mono
 
-/**
- * can replaced by ReactiveCrudRepository, like jpa repository
- *
- * @see org.springframework.data.repository.reactive.ReactiveCrudRepository
- */
 @Repository
-class ProductRepository(private val databaseClient: DatabaseClient) {
-  fun findAll(): Flux<Product> = databaseClient.select().from<Product>().fetch().all()
-  fun getOne(id: Int): Mono<Product> = databaseClient.select().from<Product>()
-      .matching(
-          where("id").`is`(id)
-      )
-      .fetch().first()
-}
+interface ProductRepository : ReactiveCrudRepository<Product, Long>
