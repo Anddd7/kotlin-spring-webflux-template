56c27385d3f5f4032ee49ba46c7c17e278b990cc
Anddd7 <liaoad_space@sina.com>
2020-01-22 15:33:10 +0800

    implement with curd repo

--- src/main/kotlin/com/github/anddd7/repository/AuthorRepository.kt
+++ src/main/kotlin/com/github/anddd7/repository/AuthorRepository.kt
@@ -1,18 +1,8 @@
 package com.github.anddd7.repository
 
 import com.github.anddd7.entity.Author
-import org.springframework.data.r2dbc.core.DatabaseClient
-import org.springframework.data.r2dbc.core.from
-import org.springframework.data.r2dbc.query.Criteria.where
+import org.springframework.data.repository.reactive.ReactiveCrudRepository
 import org.springframework.stereotype.Repository
 
 @Repository
-class AuthorRepository(private val databaseClient: DatabaseClient) {
-  fun findById(id: Int) = databaseClient
-      .select().from<Author>()
-      .matching(
-          where("id").`is`(id)
-      )
-      .fetch()
-      .first()
-}
+interface AuthorRepository : ReactiveCrudRepository<Author, Int>
