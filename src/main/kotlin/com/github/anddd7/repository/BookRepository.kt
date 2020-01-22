0854e9244c2a25c2a4835683a1047c7ba4e913c7
Anddd7 <liaoad_space@sina.com>
2020-01-22 15:33:10 +0800

    implement with curd repo

--- src/main/kotlin/com/github/anddd7/repository/BookRepository.kt
+++ src/main/kotlin/com/github/anddd7/repository/BookRepository.kt
@@ -1,19 +1,8 @@
 package com.github.anddd7.repository
 
 import com.github.anddd7.entity.Book
-import org.springframework.data.r2dbc.core.DatabaseClient
-import org.springframework.data.r2dbc.core.from
-import org.springframework.data.r2dbc.query.Criteria
-import org.springframework.data.r2dbc.query.Criteria.where
+import org.springframework.data.repository.reactive.ReactiveCrudRepository
 import org.springframework.stereotype.Repository
 
 @Repository
-class BookRepository(private val databaseClient: DatabaseClient) {
-  fun findById(id: Int) = databaseClient
-      .select().from<Book>()
-      .matching(
-          where("id").`is`(id)
-      )
-      .fetch()
-      .first()
-}
+interface BookRepository : ReactiveCrudRepository<Book, Int>
